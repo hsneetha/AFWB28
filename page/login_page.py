8dacfdf46af0394689dc0fc9a272d6a9e0052e9c
Neetha <hsneetha@gmail.com>
2022-02-18 09:24:18 +0530

    hii

---
+++ page/login_page.py
@@ -0,0 +1,34 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+
+
+class Login_Page:
+    #declaration
+    __username_tb=(By.ID,"username")
+    __password_tb=(By.NAME,"pwd")
+    __login_btn=(By.XPATH,"//div[.='Login ']")
+    __err_msg=(By.XPATH,"//span[contains(.,'invalid')]")
+    #initialization
+    def __init__(self,driver):
+        self.driver=driver
+
+    #utilization
+    def set_username(self,un):
+        self.driver.find_element(*self.__username_tb).send_keys(un)
+
+    def set_password(self,pw):
+        self.driver.find_element(*self.__password_tb).send_keys(pw)
+
+    def click_login_button(self):
+        self.driver.find_element(*self.__login_btn).click()
+
+    def verify_err_msg_is_displayed(self, wait):
+        try:
+            wait.until(expected_conditions.visibility_of_element_located(self.__err_msg))
+            print('Err Msg is displayed')
+            msg = self.driver.find_element(*self.__err_msg).text
+            print(msg)
+            return True
+        except:
+            print('Err Msg is NOT displayed')
+            return False
---
+++ test_script/test_valid_login.py
@@ -0,0 +1,30 @@
+import pytest
+
+from generic.base_test import BaseTest
+from generic.excel import Excel
+from page.enter_time_track_page import Enter_Time_Track_Page
+from page.login_page import Login_Page
+
+
+class Test_Valid_Login(BaseTest):
+    @pytest.mark.run(order=1)
+    def test_valid_login(self):
+        un=Excel.get_data(self.xl_path,"ValidLogin",2,1)
+        pw=Excel.get_data(self.xl_path,"ValidLogin",2,2)
+
+# 1.	Enter valid username- admin
+        login_page=Login_Page(self.driver)
+        login_page.set_username(un)
+
+# 2.	Enter valid password-manager
+        login_page.set_password(pw)
+
+# 3.	Click login button
+        login_page.click_login_button()
+
+# 4.	Verify that home page is displayed
+        ett_page=Enter_Time_Track_Page(self.driver)
+        result=ett_page.verify_ett_page_is_displayed(self.wait)
+        assert result
+
+        #self.driver.close()
